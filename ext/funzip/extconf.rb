c75ed36262a05e51b856a4e52ce846307953e302
Peter Winton <pwinton@indigobio.com>
2015-11-03 13:14:17 -0500

    Added include path for homebrew on Mac

--- ext/funzip/extconf.rb
+++ ext/funzip/extconf.rb
@@ -2,6 +2,6 @@ require 'mkmf'
 extension_name = 'funzip'
 dir_config(extension_name)
 have_library('zip', 'zip_open')
-$CPPFLAGS += ' -Wall -Werror -Wno-format-security -std=c++11 -O3 -fno-strict-aliasing -flto'
+$CPPFLAGS += ' -Wall -Werror -Wno-format-security -std=c++11 -O3 -fno-strict-aliasing -flto -I/usr/local/lib/libzip/include'
 
 create_makefile(extension_name)
